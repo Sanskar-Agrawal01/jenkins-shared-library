2520be139a8c442c5355670a4166d60d4569452a
Sanskar Agrawal <agrawalss_15@rknec.edu>
2026-03-08 16:50:22 +0530

    Rename DockerHubUser parameter to dockerHubUser

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call(String projectName, String tag, String DockerHubUser){
+def call(String projectName, String tag, String dockerHubUser){
     echo "Code Build Stage"
     sh "docker build -t ${dockerHubUser}/${projectName}:${tag} ."
 }
